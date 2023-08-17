b5dfe41c46ede5caba9f508e15eedc1f32a06939
chenriqueweb <carlos.henrique.mobile@gmail.com>
2023-08-17 15:16:54 -0300

    Versão Inicial

---
+++ src/main/java/br.via.com.consumer.api/Consumer.java
@@ -0,0 +1,41 @@
+package br.via.com.consumer.api;
+
+import br.com.via.consumer.config.KafkaConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static br.com.via.consumer.config.KafkaConfig.getProperties;
+import static br.com.via.consumer.config.KafkaConsumerConfig.propertiesConsumer;
+
+public class Consumer {
+
+    @Autowired
+    KafkaConsumerConfig kafkaConsumerConfig;
+
+    public static void main(String[] args) throws IOException {
+
+        // Teste 1 - Topico com uma unica particao - compras.do.cliente
+        var consumer = new KafkaConsumer<String, String>(propertiesConsumer());
+
+        Properties prop = getProperties();
+        consumer.subscribe(Collections.singletonList(prop.getProperty("kafka.producer.topic")));
+
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+
+            for (var record : records) {
+                System.out.println("Compra nova: ");
+                System.out.println("Key: " + record.key());
+                System.out.println("Valor: " + record.value());
+                System.out.println("Offset: " + record.offset());
+                System.out.println("Partition: " + record.partition());
+                System.out.println("-------------------------------------");
+            }
+        }
+    }
+}
