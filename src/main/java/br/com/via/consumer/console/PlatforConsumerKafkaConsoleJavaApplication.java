ae5ab6c2be29414062fae7e124492e9ff85db2ad
chenriqueweb <carlos.henrique.mobile@gmail.com>
2023-08-17 17:33:49 -0300

    Versão Inicial

--- src/main/java/br/com/via/consumer/console/PlatforConsumerKafkaConsoleJavaApplication.java
+++ src/main/java/br/com/via/consumer/console/PlatforConsumerKafkaConsoleJavaApplication.java
@@ -1,16 +1,9 @@
 package br.com.via.consumer.console;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-
-@SpringBootApplication
 public class PlatforConsumerKafkaConsoleJavaApplication {
-    public static void main(String[] args) throws IOException {
-        SpringApplication.run(PlatforConsumerKafkaConsoleJavaApplication.class, args);
+    public static void main(String[] args) {
+        System.out.println("Consumer Kafka - Console");
 
         new Consumer();
     }
-
 }
